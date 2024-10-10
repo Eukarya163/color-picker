2fafb021ad44f0cc62114b6f70db5cf6a4144a49
Eukarya163 <Eukarya163@users.noreply.github.com>
2024-10-11 08:00:31 +0900

    ch3-8

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ const text = document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 
 text.textContent = `カラーコード： ${color.value}` ;
+color.addEventListenerLinstener('input', colorBg);
