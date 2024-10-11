480f0055c592efce370423a7a5484b64dc3e9d28
Eukarya163 <Eukarya163@users.noreply.github.com>
2024-10-11 10:59:05 +0900

    完成

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 const text = document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 
+text.textContent = 'を検索';
+
 const colorBg = () => {
-    text.textContent = `カラーコード： ${color.value}`;
     document.body.style.backgroundColor = color.value;
+
+    if (color.value === '#ffffff') {
+        text.textContent = `: ${color.value} (white)`;
+    } else if (color.value === '#000000') {
+        text.tectContent = `: ${color.value} (black)`
+    }
+    else{
+        text.textContent = `: ${color.value}`;
+    }
 };
 
 color.addEventListener('input', colorBg);
